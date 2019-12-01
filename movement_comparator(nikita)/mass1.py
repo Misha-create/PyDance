2f827782fbbc66a58b6a2a839cb5bc190902017f
Nikprokop <55061626+Nikprokop@users.noreply.github.com>
2019-12-01 22:59:19 +0300

    2

--- movement_comparator(nikita)/mass1.py
+++ movement_comparator(nikita)/mass1.py
@@ -1,36 +1,50 @@
 # coding: utf-8
-import random
-import time
 
-n = 5
-m = 3
-type_1 = ["a", "b", "c", "d", "e"]
-time_begin = [1, 3, 5, 6, 5]
-time_end = [3, 5, 7, 8, 9]
-type_id = ["a", "b", "c", "d", "e"]
-time_begin_id = [1,2,3,6,5]
-time_end_id = [4,4,5,8,9]
+Ideal_file = open("Ideal.txt", "r")
+Player_file = open('Player.txt')
+Player_massive = Player_file.read().split()
+Ideal_massive = Ideal_file.read().split()
+
+type_1 = []
+time_begin = []
+time_end = []
+type_id = []
+time_begin_id = []
+time_end_id = []
 Player = []
 Ideal = []
 points = 0
+
+for i in range(len(Player_massive)):
+    if (i % 3 == 0):
+        type_1.append(Player_massive[i])
+    elif (i % 3 == 1):
+        time_begin.append(int(Player_massive[i]))
+    elif (i % 3 == 2):
+        time_end.append(int(Player_massive[i]))
+
+for i in range(len(Ideal_massive)):
+    if (i % 3 == 0):
+        type_id.append(Ideal_massive[i])
+    elif (i % 3 == 1):
+        time_begin_id.append(int(Ideal_massive[i]))
+    elif (i % 3 == 2):
+        time_end_id.append(int(Ideal_massive[i]))
+
+print(type_1)
+print(time_begin)
+print(time_end)
+print(type_id)
+print(time_begin_id)
+print(time_end_id)
 
 Player.append(type_1)
 Player.append(time_begin)
 Player.append(time_end)
 
-for i in range(len(Player[0])):
-    for j in range(len(Player)):
-        print(Player[j][i], end=' ')
-    print()
-
 Ideal.append(type_id)
 Ideal.append(time_begin_id)
 Ideal.append(time_end_id)
-
-for i in range(len(Ideal[0])):
-    for j in range(len(Ideal)):
-        print(Ideal[j][i], end=' ')
-    print()
 
 for i in range(len(Ideal[0])):
     if (Player[0][i] == Ideal[0][i]):
@@ -41,4 +55,5 @@ for i in range(len(Ideal[0])):
             points += 0.5
         elif (mid_player <= mid_ideal):
             points += 1
+
 print(points)
